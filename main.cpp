0083b1430cf2c576314fc8ba252c41158e42db4c
DexterW1 <dexteryiumingwong@gmail.com>
2020-11-30 17:28:23 -0800

    add initial files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "base.hpp"
+#include "div.h"
+#include "pow.h"
+#include "add.h"
+#include "op.hpp"
+#include "sub.h"
+#include "rand.h"
+#include "mult.h"
+int main(){
+    
+}
